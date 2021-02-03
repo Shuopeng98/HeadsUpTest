d8f604bf6bb447e005f7a10954744e7527da87df
Shuopeng98 <lee3014@icloud.com>
2021-02-03 11:00:45 +0800

    test

---
+++ HeadsUp01/assets/Script/Btn_Home.js
@@ -0,0 +1,36 @@
+// Learn cc.Class:
+//  - https://docs.cocos.com/creator/manual/en/scripting/class.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        intro_content: cc.Node,
+        dataNode: cc.Node
+    },
+
+    onLoad() {
+        this.audioNode = cc.find('audioNode').getComponent('AudioController')
+    },
+
+    onClickButton: function(sender, str) {
+        this.audioNode.playAudio('btn')
+        switch (str) {
+            case 'start':
+                this.audioNode.playAudio('music')
+                cc.director.loadScene('WordsChoice')
+                break
+            case 'intro_open':
+                this.intro_content.active = true
+                break
+            case 'intro_close':
+                this.intro_content.active = false
+                break
+        }
+
+    }
+});
