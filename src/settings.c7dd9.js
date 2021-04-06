bc23c17eeb2e3f2d89def5938fb6cf3dc141b922
Shuopeng98 <lee3014@icloud.com>
2021-04-06 21:45:30 +0800

    1

---
+++ src/settings.c7dd9.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/Home.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"f1413",resources:"e1e3b",main:"0a373"}};
